fab31ffb6d822403baab18de2274960bf63c07fe
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2025-03-06 19:51:09 +0800

    feat: stack

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,25 @@
 
 mod lang_items;
 
-use core::arch::global_asm;
+use core::arch::{asm, global_asm};
 global_asm!(include_str!("entry.asm"));
+
+// #[unsafe(no_mangle)]
+// #[unsafe(link_section = ".text.entry")]
+// #[naked]
+// fn _start() -> ! {
+//     unsafe {
+//         asm! {
+//             "la sp, boot_stack_top"
+//         }
+//     }
+//     main();
+//     loop {}
+// }
+
+#[unsafe(no_mangle)]
+fn rust_main() -> () {
+    unsafe {
+        asm!("li ra, 100");
+    }
+}
